ab40f0f47588d652dc18019cb53858ee70750758
me <shirefelageiry@gmail.com>
2025-03-03 03:18:21 +0200

    switch between products

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { Location } from '@angular/common';
 import { Iproduct } from '../../models/iproduct';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-details',
@@ -13,15 +15,43 @@ export class DetailsComponent implements OnInit {
   
   currentId:number=0;
   product:Iproduct| null=null;
-  constructor(private _activateRoute: ActivatedRoute, private _productService: ProductService) {
+  idArr: number[] = [];
+  
+  constructor(private _activateRoute: ActivatedRoute, private _productService: ProductService, 
+    private _location: Location, private _router: Router) {
 
+    this.idArr = this._productService.mapProductsToId();
     
-   }
+  }
   ngOnInit(): void {
-   this.currentId=Number( this._activateRoute.snapshot.paramMap.get('id'));
-    this.product=this._productService.getProductById(this.currentId);
+      this._activateRoute.paramMap.subscribe((params)=>{
+      this.currentId=Number(params.get('id'));
+      this.product=this._productService.getProductById(this.currentId);
+    });
+
+      
+  //  this.currentId=Number( this._activateRoute.snapshot.paramMap.get('id'));
+  //   this.product=this._productService.getProductById(this.currentId);
   }
 
+  goBack(){
+    // i want to go back to the previous page
+    this._location.back();
+  }
 
+  next(){
+    
+    let currrentIndex=this.idArr.findIndex((id)=>id===this.currentId);
+    if (currrentIndex!=this.idArr.length-1)
+    this._router.navigateByUrl(`/details/${this.idArr[currrentIndex+1]}`);
 
+  }
+
+  prev(){
+    
+    let currrentIndex=this.idArr.findIndex((id)=>id===this.currentId);
+    if (currrentIndex!=0){
+      this._router.navigateByUrl(`/details/${this.idArr[currrentIndex-1]}`);
+    }
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -15,8 +15,8 @@ export class ProductService {
           { id: 2, name: "Smartphone", price: 499.99, quantity: 25, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 2 },
           { id: 3, name: "apple watch", price: 79.99, quantity: 50, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 3 },
           { id: 4, name: "Dell", price: 299.99, quantity: 1, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 1 },
-          { id: 4, name: "lenovo", price: 299.99, quantity: 15, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 3 },
-          { id: 5, name: "Smartwatch", price: 149.99, quantity: 0, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 2 },
+          { id: 5, name: "lenovo", price: 299.99, quantity: 15, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 3 },
+          { id: 8, name: "Smartwatch", price: 149.99, quantity: 0, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 2 },
           { id: 6, name: "Smartwatch", price: 149.99, quantity: 0, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 2 },
           { id: 7, name: "Smartwatch", price: 149.99, quantity: 0, imgUrl: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1", categoryId: 2 }
       ];
@@ -37,6 +37,10 @@ export class ProductService {
   getProductById(id:number):Iproduct |null{
      let found=this.products.find(prd=>prd.id===id);
      return found ? found :null;
+  }
+
+  mapProductsToId():number[]{
+    return this.products.map(prd=>prd.id);
   }
 
 }
